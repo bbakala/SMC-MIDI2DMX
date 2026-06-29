96ddeb7117f09ae22ceba5cd66b8f4b048f50917
bbakala <48054488+bbakala@users.noreply.github.com>
2026-06-29 15:22:13 +0200

    Add project version header

--- Core/Version.h
+++ Core/Version.h
@@ -1,7 +1,18 @@
 #pragma once
 
+//====================================================
+// SMC-MIDI2DMX
+// Project version
+//====================================================
+
 #define PROJECT_NAME        "SMC-MIDI2DMX"
-#define PROJECT_VERSION     "04_01"
+
 #define VERSION_MAJOR       4
-#define VERSION_REVISION    1
-#define CORE_VERSION        "Core_04_01"
+#define VERSION_MINOR       1
+#define VERSION_PATCH       0
+
+#define PROJECT_VERSION     "04_01"
+#define VERSION_TAG         "v04_01"
+
+#define BUILD_DATE          __DATE__
+#define BUILD_TIME          __TIME__
