d57aa6b453217839a0dfea0e272ed7bd42d7dbc6
bbakala <48054488+bbakala@users.noreply.github.com>
2026-06-30 22:06:08 +0200

    Add shared DMX configuration

---
+++ Core/DmxConfig.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <Arduino.h>
+#include "Config.h"
+
+// DMX512
+constexpr uint32_t DMX_BAUD = 250000;
+
+// DMX buffer obsahuje start code + 128 logických kanálů.
+constexpr uint16_t DMX_BUFFER_SIZE = DMX_LOGICAL_COUNT + 1;
+
+// DMX start code
+constexpr uint8_t DMX_START_CODE = 0x00;
